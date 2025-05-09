0e8a772deea832d855bb52cac0516111d74a6999
Avenger <vonjy@numherit.com>
2025-05-09 13:32:59 +0300

    init suivi compte

--- src/app/views/modules/adm/geston-compte-principal/suivi-compte-principal/suivi-compte-principal.component.ts
+++ src/app/views/modules/adm/geston-compte-principal/suivi-compte-principal/suivi-compte-principal.component.ts
@@ -1,15 +1,90 @@
+import { formatDate } from '@angular/common';
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'app/services/http.service';
+import { PassageService } from 'app/services/table/passage.service';
+import { environment } from 'environments/environment';
+import { Subscription } from 'rxjs';
+import { Translatable } from 'shared/constants/Translatable';
 
 @Component({
   selector: 'app-suivi-compte-principal',
   templateUrl: './suivi-compte-principal.component.html',
   styleUrls: ['./suivi-compte-principal.component.scss']
 })
-export class SuiviComptePrincipalComponent implements OnInit {
+export class SuiviComptePrincipalComponent extends Translatable implements OnInit {
 
-  constructor() { }
+    endpoint = "";
 
-  ngOnInit(): void {
-  }
+    typeCompte: string = "0";
+    dateDebut: string;
+    dateFin: string;
+
+    rows: any[] = [];
+    currentPage = 1;
+    lastPage = 1;
+    total = 0;
+    solde: String = "0";
+    soldeCarte: String = "0";
+
+    loadingData = false;
+
+    constructor(private passageService: PassageService,private httpService : HttpService) {
+        super();
+    }
+
+    ngOnInit(): void {
+        this.passageService.appelURL(null);
+        this.endpoint = environment.suivi_compte;
+        this.getDataSuivi(this.currentPage);
+    }
+
+    getDataSuivi(page: number)
+    {
+        const dateDebutFormatted = this.dateDebut
+            ? formatDate(this.dateDebut, 'yyyy-MM-dd', 'en-US')
+            : undefined;
+
+        const dateFinFormatted = this.dateFin
+            ? formatDate(this.dateFin, 'yyyy-MM-dd', 'en-US')
+            : undefined;
+
+        let url = `${this.endpoint}?page=${page}&per_page=2&__order__=desc,date_transaction`;
+        if (dateDebutFormatted) url += `&date_debut=${dateDebutFormatted}`;
+        if (dateFinFormatted) url += `&date_fin=${dateFinFormatted}`;
+        if (this.typeCompte != "0") url += `&where=releve_des_comptes.wallet_carte|e|${this.typeCompte}`;
+        this.loadingData = true;
+
+        this.httpService.get(url).subscribe({
+            next: (res) => {
+                this.loadingData = false;
+                if(res['code'] == 200) {
+                    const resultData = res['data'];
+                    this.rows = resultData.data;
+                    this.currentPage = resultData.current_page;
+                    this.lastPage = resultData.last_page;
+                    this.total = resultData.total;
+                }
+            },
+        });
+    }
+
+    nextPage() {
+        if (this.currentPage < this.lastPage) {
+            this.getDataSuivi(this.currentPage+= 1);
+        }
+    }
+    
+    prevPage() {
+        if (this.currentPage > 1) {
+          this.getDataSuivi(this.currentPage - 1);
+        }
+    }
+
+    fitlerSuivi()
+    {
+        this.currentPage = 1;
+        this.getDataSuivi(this.currentPage);
+    }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,11 +13,32 @@ export const environment = {
   authorityModule : 'authority_module',
   authoritySousModule : 'authority_sousModule',
 
+  //** Parametrage */
   module :'parametrage/module',
   sous_module :'parametrage/sousmodule',
   liste_module_active :'parametrage/consult/module/liste_module_active',
   type_bureau :'parametrage/type_bureau',
   type_profil :'parametrage/type_profil',
+  profil :'parametrage/profil',
+  liste_profil_active :'parametrage/consult/profil/allActivatedProfil',
+  liste_type_profil_active :'parametrage/consult/type_profil/liste_type_profil_active',
+  utilisateur :'parametrage/user',
+  liste_type_bureau_active :'parametrage/consult/type_bureau/type_de_bureaux_active',
   getSoldeUser : 'gestion_bureau/consult/bureaux/get_solde',
   regenerer_mdp :'parametrage/user/regenerer_password',
+  profilage : 'parametrage/profilage',
+  action : 'parametrage/action',
+  appendroute : 'parametrage/settings/appendroute',
+  generateroute : 'parametrage/settings/generateroute',
+  
+  //** Gestion bureau */
+  liste_bureau_active :'gestion_bureau/consult/bureaux/liste_bureaux_active',
+  bureau : 'gestion_bureau/bureaux',
+
+  //** Ressource */
+  province : 'resource/province/liste_province',
+  departement : 'resource/departement/liste_departement',
+
+  //** Gestion compte principal */
+  suivi_compte : "gestion_compte_principal/suivi_compte"
 };
