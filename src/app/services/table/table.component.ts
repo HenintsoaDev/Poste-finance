38913dc81bd10df84b0c8a7f998ed01bc4a002be
Avenger <vonjy@numherit.com>
2025-05-12 17:02:20 +0300

    remove console hents

--- src/app/services/table/table.component.ts
+++ src/app/services/table/table.component.ts
@@ -414,7 +414,6 @@ export class TableComponent extends Translatable {
         const statutItem = cell.find(item => 'statut' in item);
         // Récupère la valeur
         const statut = statutItem ? (statutItem as { statut: number }).statut : undefined;
-        console.log("xxx", statut);
         return statut;
             
     }
