40e5639edcfd2ffd4f28a18701058dd3e3d13f56
Avenger <vonjy@numherit.com>
2025-06-03 16:09:03 +0300

    filtre and pront

--- src/app/views/modules/reporting/suivi-transaction/historique-transaction/historique-transaction.component.ts
+++ src/app/views/modules/reporting/suivi-transaction/historique-transaction/historique-transaction.component.ts
@@ -1,9 +1,10 @@
 import { DatePipe } from '@angular/common';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatExpansionPanel } from '@angular/material/expansion';
 import { AuthService } from 'app/services/auth.service';
 import { PassageService } from 'app/services/table/passage.service';
 import { environment } from 'environments/environment';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { CountryISO,SearchCountryField } from 'ngx-intl-tel-input';
 import { ToastrService } from 'ngx-toastr';
 import { Subscription } from 'rxjs';
@@ -15,6 +16,8 @@ import { Translatable } from 'shared/constants/Translatable';
   styleUrls: ['./historique-transaction.component.scss']
 })
 export class HistoriqueTransactionComponent extends Translatable implements OnInit {
+
+    @ViewChild('detailTransaction') detailTransaction: TemplateRef<any> | undefined; 
 
     endpoint = "";
     header = [
@@ -50,7 +53,24 @@ export class HistoriqueTransactionComponent extends Translatable implements OnIn
     listIcon = [{'icon' : 'info','action' : 'info','tooltip' : this.__('global.tooltip_detail'),'autority' : '',},]
     searchGlobal = ['transaction.num_transac']
 
-    titleModal: string;
+    listTransactions = [];
+    date_transaction: string;
+    num_transac: string;
+    client: string;
+    service: string;
+    montant: string;
+    commission: string;
+    montant_ttc: string;
+    effectue_par: string;
+    agence: string;
+    wallet_carte: string;
+
+    titleModal: string = "";
+    modalRef?: BsModalRef;
+    idTransaction: number;
+    transactions: any = [];
+    list_transactions_totaux: any;
+
     num_compte: any;
     type_recherche: any;
     telephone: any;
@@ -80,7 +100,8 @@ export class HistoriqueTransactionComponent extends Translatable implements OnIn
         private toastr: ToastrService, 
         private passageService: PassageService,
         private datePipe: DatePipe, 
-        private authService : AuthService
+        private authService: AuthService,
+        private modalService: BsModalService,
     ) {
         super();
     }
@@ -101,7 +122,17 @@ export class HistoriqueTransactionComponent extends Translatable implements OnIn
         this.endpoint = environment.baseUrl + '/' + environment.reporting_historique_transaction;
         /***************************************** */
 
-
+        this.subscription = this.passageService.getObservable().subscribe(event => {
+  
+            if( event.data){
+                this.idTransaction = event.data.id;
+  
+                if (event.data.action == 'info') {
+                    this.openModalTransaction();
+                }
+                this.passageService.clear();  // ==> à implémenter dans ton service
+            }
+        });
 
     }
 
@@ -123,7 +154,7 @@ export class HistoriqueTransactionComponent extends Translatable implements OnIn
         if(this.type_recherche != undefined) filtre_type_recherche = "&type_recherche="+type;
         
         let filtre_wallet_carte = '';
-        if(this.typeCompte != "2") filtre_wallet_carte = "&wallet_carte="+this.typeCompte;
+        if(this.typeCompte != "2") filtre_wallet_carte = "&where=transaction.wallet_carte|e|"+this.typeCompte;
         
         let filtre_telephone = '';
         if(this.telephone != undefined) {
@@ -163,6 +194,50 @@ export class HistoriqueTransactionComponent extends Translatable implements OnIn
         this.panel.close();
     }
 
+    openModalTransaction()
+    {
+        this.recupererDonnee();
+
+        if (this.modalRef) {
+            this.modalRef.hide();
+        }
+
+        this.titleModal = this.__('transaction_jour.detail');
+        this.modalRef = this.modalService.show(this.detailTransaction, {
+            backdrop: 'static',
+            keyboard: false,
+            class: 'modal-lg'
+        });
+    }
+
+    recupererDonnee()
+    {
+        // Récupérer la liste affichée dans le tableau depuis le localStorage.
+        const storedData = localStorage.getItem('data');
+        let result : any;
+        if (storedData) result = JSON.parse(storedData);
+        this.listTransactions = result.data;
+
+        // Filtrer le tableau par rapport à l'ID et afficher le résultat dans le formulaire.
+        let res = this.listTransactions.filter(_ => _.id == this.idTransaction);
+
+        if (res.length != 0) { 
+            let transactionSelected = res[0];
+            this.date_transaction = transactionSelected.date_transaction;
+            this.num_transac = transactionSelected.num_transac;
+            this.client = transactionSelected.client;
+            this.telephone = transactionSelected.telephone;
+            this.service = transactionSelected.service;
+            this.montant = transactionSelected.montant;
+            this.commission = transactionSelected.commission;
+            this.montant_ttc = transactionSelected.montant_ttc;
+            this.effectue_par = transactionSelected.effectue_par;
+            this.agence = transactionSelected.agence;
+            this.wallet_carte = transactionSelected.wallet_carte;
+        }
+
+    }
+
     /** PHONE NUMBER */
     telInputObject(m:any){
         this.objetPhone = m.s
@@ -181,6 +256,87 @@ export class HistoriqueTransactionComponent extends Translatable implements OnIn
     
     getNumber(obj : any) {
         this.telephone = obj;
+    }
+
+    closeModal() {
+        this.modalRef?.hide();
+    }
+
+    exportExcel() {
+        const storedData = localStorage.getItem('data');
+        let result: any;
+        
+        if (storedData) result = JSON.parse(storedData);
+        this.transactions = result.data;
+        this.list_transactions_totaux = result;
+
+        this.authService.exportExcel(this.print(this.transactions), this.__('transaction_jour.title')).then(
+            (response: any)=>{
+                  let a = document.createElement("a"); 
+                  a.href = response.data;
+                  a.download = `${this.__('transaction_jour.title')}.xlsx`;
+                  a.click(); 
+            },
+            (error:any)=>{
+              console.log(error)
+            }
+        );
+    }
+    
+    exportPdf() {
+        const storedData = localStorage.getItem('data');
+        let result: any;
+        
+        if (storedData) result = JSON.parse(storedData);
+        this.transactions = result.data;
+        this.list_transactions_totaux = result;
+
+        this.authService.exportPdf(this.print(this.transactions),this.__('transaction_jour.title')).then(
+            (response: any)=>{},
+            (error:any)=>{console.log(error)}
+        );
+    }
+
+    print(transactions:any[]){
+
+        let tab = transactions.map((transac: any, index: number) => {
+          let t: any = {};
+            t[this.__('global.date')] = transac.date_transaction;
+            t[this.__('global.num_transac')] = transac.num_transac;
+            t[this.__('global.nom_client')] = transac.client;
+            //t[this.__('utilisateur.telephone')] = transac.telephone;
+            t[this.__('global.service')] = transac.service;
+            t[this.__('global.montant')] = transac.montant;
+            t[this.__('service.frais')] = transac.commission;
+            t[this.__('global.montant_brut')] = transac.montant_ttc;
+            t[this.__('global.effectue_par')] = transac.effectue_par;
+            t[this.__('global.agence')] = transac.agence;
+            t[this.__('suivi_compte.type_compte')] = transac.wallet_carte;
+                  
+            return t;
+        });
+
+
+
+        // puis ajouter les totaux à la fin
+        tab.push({
+            [this.__('global.date')]: '',
+            [this.__('global.num_transac')]: this.__('global.total_montant'),
+            [this.__('global.nom_client')]: this.list_transactions_totaux?.total_montant ?? 0,
+            //[this.__('utilisateur.telephone') ]: '',
+            
+            [this.__('global.service')]: '',
+            [this.__('global.montant') ]: this.__('global.total_commission'),
+            [this.__('service.frais') ]: this.list_transactions_totaux?.total_commission ?? 0,
+            [this.__('global.montant_brut') ]: '',
+            
+            [this.__('global.effectue_par')]: this.__('global.total_ttc'),
+            [this.__('global.agence')]: this.list_transactions_totaux?.total_ttc ?? 0,
+            [this.__('suivi_compte.type_compte')]: '',
+        });
+
+        return tab;
+    
     }
 
 }
